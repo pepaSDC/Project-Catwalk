ee8a914d112e0d910da73da8d555a7b088a35bcf
Thanh Ha <thanh3203@gmail.com>
2021-12-22 11:50:54 -0800

    Featuring Framework

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', 'jsx']
+  }
+};
