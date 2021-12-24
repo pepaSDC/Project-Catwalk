afe3a42ce5a3bf42a0529fdf8695295b89cb29b1
Thanh Ha <thanh3203@gmail.com>
2021-12-23 21:43:46 -0800

    changed product id to one that exists

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -11,7 +11,7 @@ const Routing = () => {
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Navigate replace to="/products/2"/>}/>
+          <Route path="/" element={<Navigate replace to="/products/40344"/>}/>
           <Route path="/products/:id" element={<ProductDetail/>}/>
         </Routes>
       </div>
