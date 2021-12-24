9d2bd7d7f647cc28547d546378226fcf60cdf1ca
Thanh Ha <thanh3203@gmail.com>
2021-12-24 11:40:56 -0800

    refactored express to use express.router to separate handlers for different end points

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const api = require('../src/api.js');
+
 const reviews = require('./router/reviews.js');
 const QA = require('./router/QAs.js');
-const api = require('../src/api.js');
+const products = require('./router/products.js');
 
 
 const app = express();
@@ -9,46 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/products', (req, res) => {
-  api.getAllProducts( (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      res.send(result.data);
-    }
-  });
-});
-
-app.get('/products/:id', (req, res) => {
-  api.getProductInfo( req.params.id, (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      res.send(result.data);
-    }
-  });
-});
-
-app.get('/products/:id/styles', (req, res) => {
-  api.getProductStyles( req.params.id, (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      res.send(result.data);
-    }
-  });
-});
-
-app.get('/products/:id/related', (req, res) => {
-  api.getRelatedProducts( req.params.id, (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      res.send(result.data);
-      // returns an array of products_id that are related to each other
-    }
-  });
-});
+app.use('/products', products);
 
 app.use('/reviews', reviews);
 
