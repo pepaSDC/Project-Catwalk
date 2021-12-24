c6d1e80568b023ce0b4c0ccad81d3361bb7157f5
Thanh Ha <thanh3203@gmail.com>
2021-12-23 17:35:43 -0800

    added a route for reviews to handle requests being sent to reviews endpoint

---
+++ server/router/reviews.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const api = require('../../src/api.js');
+
+
+router.get('/', (req, res) => {
+  const product_id = parseInt(req.query.products_id);
+  const sort = req.query.sort;
+
+  api.getReviews(product_id, sort, (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(result.data);
+    }
+  });
+});
+
+module.exports = router;
