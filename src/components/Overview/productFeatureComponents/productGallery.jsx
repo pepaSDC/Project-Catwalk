9c540a9ee9b77b79ea74dd85913bcfa289f159db
timnevada <timnevada@gmail.com>
2021-12-27 18:40:22 -0800

    working on product Gallery

--- src/components/Overview/productFeatureComponents/productGallery.jsx
+++ src/components/Overview/productFeatureComponents/productGallery.jsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { GlobalContext } from '../../../context/GlobalState.js'
+const axios = require('axios');
 
-const productGalleryStyle = {
+let productGalleryStyle = {
   display: 'flex',
-  flexGrow: 8
+  flexGrow: 8,
 }
 
 export const ProductGallery = () => {
   const { currentProductId } = useContext(GlobalContext);
+
+  axios.get(`localhost:3000/products/${currentProductId}/styles`)
+  .then((productStyles) => {
+    console.log('current product styles: ', productStyles);
+  })
+  .catch((error) => {
+    console.error(error);
+  })
 
   return (
     <div
