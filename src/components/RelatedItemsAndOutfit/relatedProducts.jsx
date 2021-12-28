cc60ecc6a832cc4a012779ab4b82100ec1a084bc
Michael McIntyre <msmcintyre20@gmail.com>
2021-12-27 17:08:59 -0700

    deleted extra related items

--- src/components/RelatedItemsAndOutfit/relatedProducts.jsx
+++ src/components/RelatedItemsAndOutfit/relatedProducts.jsx
@@ -4,6 +4,7 @@ import { GlobalContext } from '../../context/GlobalState.js'
 export const RelatedProducts = () => {
   const { currentProductId } = useContext(GlobalContext);
   console.log('current p id from related products', currentProductId)
+
   return (
     <div>
       <div className="app">Hello From RelatedProducts</div>
