29185e0f5437714e779d4ae682a7e10e4f62b4e8
Michael McIntyre <msmcintyre20@gmail.com>
2021-12-28 18:12:44 -0700

    props are being pased to card componets

--- src/components/RelatedItemsAndOutfit/relatedProducts.jsx
+++ src/components/RelatedItemsAndOutfit/relatedProducts.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { GlobalContext } from '../../context/GlobalState.js';
 import axios from 'axios';
 import Promise from 'bluebird';
+import RelatedProductsCardCarousel from './RelatedProductsCardCarousel.jsx';
 
 export const RelatedProducts = () => {
   //global state import
@@ -22,29 +23,46 @@ export const RelatedProducts = () => {
         url: `/products/${id}/related`
       })
   }
+  //this call is going to get the images for each product, need to filter a bit
+  const getProductStyles = (id) => {
+    return axios({
+      method: 'GET',
+      url: `/products/${id}/styles`
+    })
+  }
+
+
+
   //function to update state with an array of currentId return promises
   const updateRelatedProductInfoState = (id) => {
     const promiseArr = getRelatedIDs(id)
     .then(ids => {
       return ids.data.map(curId => {
-        return getProduct(curId);
+        return axios
+                .all([getProduct(curId), getProductStyles(curId)])
       })
     })
+
     return Promise.all(promiseArr);
   }
+
 
   useEffect(() =>{
     //update the related products state is called an used
     updateRelatedProductInfoState(currentProductId)
-    .then(pInfo => {
-      setRelatedProductArray(pInfo);
+    .then(pInfoStyles => {
+      var dataOnly = pInfoStyles.map((cur) => {
+      return [cur[0].data, cur[1].data.results[0].photos[0].thumbnail_url];
+      })
+      setRelatedProductArray(dataOnly);
     })
 
   },[currentProductId]);
-
+  console.log('Related Product Array in State:::' , relatedProductArray);
   return (
     <div>
       <div className="app">Hello From RelatedProducts</div>
+      <RelatedProductsCardCarousel relatedProductArray={relatedProductArray}/>
     </div>
   );
 }
