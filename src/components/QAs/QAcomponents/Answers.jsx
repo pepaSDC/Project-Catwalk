925b586eb8638e61367315b87bc63059d947053b
Thanh Ha <thanh3203@gmail.com>
2021-12-29 15:18:33 -0800

    added an Answer component to format each answer

--- src/components/QAs/QAcomponents/Answers.jsx
+++ src/components/QAs/QAcomponents/Answers.jsx
@@ -1,22 +1,20 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-const styles = {
-  fontSize: '13px',
-  color: 'gray'
-};
+import Answer from './Answer.jsx';
 
 const Answers = (props) => {
   const [count, setCount] = useState(2);
-  const [answers, setAnswers] = useState( () => {
-    return props.answers.slice(0, count);
-  });
+  const [answers, setAnswers] = useState([]);
+  useEffect( () => {
+    setAnswers( () => {
+      return props.answers.slice(0, count);
+    })
+  }, [count]);
   return (
     <div>
       {answers.map( ans => {
-        return <div key={ans.answer_id}>A: <span style={styles}>{ans.body}</span>
-        </div>;
+        return <Answer key={ans.answer_id} answer={ans}/>;
       })}
     </div>
   );
