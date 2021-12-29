a649245dece2b0a13f20d1a9fffc26dcf6a27423
Thanh Ha <thanh3203@gmail.com>
2021-12-29 11:57:43 -0800

    changed file name to QnA to escape the ambersand. Updated component to initialize state and pass down to children components

---
+++ src/components/QAs/QnA.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { GlobalContext } from '../../context/GlobalState.js';
+import axios from 'axios';
+import Search from './QAcomponents/Search.jsx';
+import Questions from './QAcomponents/Questions.jsx';
+
+const container = {
+  border: '1px solid green',
+  position: 'relative'
+};
+
+const textStyle = {
+  fontSize: '2vmin',
+  paddingTop: '10px',
+  paddingBottom: '10px'
+}
+
+export const QA = () => {
+  const { currentProductId } = useContext(GlobalContext);
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState({});
+
+  useEffect( () => {
+    axios.get(`/qa/questions?product_id=${currentProductId}`)
+      .then(response => {
+        setQuestions(response.data.results);
+        return response.data.results;
+      })
+      .then(data => {
+        let promises = data.map(q => {
+          return new Promise ( (resolve, reject) => {
+            axios.get(`/qa/questions/${q.question_id}/answers`)
+              .then(response => {
+                let answer = {
+                  questionId: q.question_id,
+                  answers: response.data.results
+                }
+                resolve(answer);
+              })
+              .catch(err => {
+                reject(err);
+              });
+          });
+        });
+        return Promise.all(promises);
+      })
+      .then(ans => {
+        let answerState = {};
+        ans.forEach( a => {
+          answerState[a.questionId] = a.answers
+        });
+        setAnswers(answerState);
+      })
+      .catch(err => console.log(err));
+  }, [currentProductId]);
+
+  return (
+    <div style={container}>
+      <div style={textStyle}>QUESTIONS & ANSWERS</div>
+      <Search />
+      <Questions questions={questions} answers={answers}/>
+    </div>
+  );
+}
