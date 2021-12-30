dee9e6f17d1bde764aad79612acee827747005e6
Alissa Luk <alissaluk1995@gmail.com>
2021-12-30 13:29:28 -0500

    fixed webpack and json package issues

--- webpack.config.js
+++ webpack.config.js
@@ -27,21 +27,6 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: false,
-            },
-          },
-        ],
-
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-
       }
 
     ]
