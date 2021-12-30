766a6d76242db459c3651cfcc1008dc3ca8b0436
Michael McIntyre <msmcintyre20@gmail.com>
2021-12-30 12:29:08 -0700

    added buttons to related products

--- src/components/RelatedItemsAndOutfit/RelatedProductsCardCarousel.jsx
+++ src/components/RelatedItemsAndOutfit/RelatedProductsCardCarousel.jsx
@@ -6,10 +6,11 @@ import './styles.css';
 
 
 const RelatedProductsCardCarousel = (props) => (
+
   <div className={'container'}>
-    <button className="arrowLeft"></button>
+    <button className="arrowLeft">&#8647;</button>
       {props.relatedProductArray.map((ele, idx) => <RelatedProduct information={ele} key={idx} itemNum={idx}/>)}
-    <button className="arrowRight"></button>
+    <button className="arrowRight">&#8649;</button>
   </div>
 )
 
--- webpack.config.js
+++ webpack.config.js
@@ -27,23 +27,7 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: false,
-            },
-          },
-        ],
-
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-
       }
-
     ]
   },
   resolve: {
