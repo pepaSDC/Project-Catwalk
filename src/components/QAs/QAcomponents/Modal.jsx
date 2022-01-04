9226dc224793fb75c2003ea3c86601ea1ea1d44b
Thanh Ha <thanh3203@gmail.com>
2022-01-04 15:48:50 -0800

    added product name on title of the modal

--- src/components/QAs/QAcomponents/Modal.jsx
+++ src/components/QAs/QAcomponents/Modal.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const Modal = ({ open, children, onClose, qBody }) => {
+const Modal = ({ open, children, onClose, qBody, product_name }) => {
   if (!open) { return null; };
   return (
     <div>
@@ -9,7 +9,7 @@ const Modal = ({ open, children, onClose, qBody }) => {
       <div className='modal'>
         <div className='title'>
           Submit your Answer
-          <div className='subtitle'>Product Name: {qBody}</div>
+          <div className='subtitle'>{product_name}: {qBody}</div>
         </div>
         {children}
       </div>
