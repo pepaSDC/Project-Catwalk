ad18d7dcf81ea7720c2b3ca25882d45635824ff5
Thanh Ha <thanh3203@gmail.com>
2022-01-05 15:01:20 -0800

    addeda message for when there are no answers for a question

--- src/components/QAs/QAcomponents/Answers.jsx
+++ src/components/QAs/QAcomponents/Answers.jsx
@@ -19,7 +19,7 @@ const Answers = (props) => {
 
   return (
     <div className='individualAns'>
-      <span>A:</span>
+      {props.answers.length > 0 ? <span>A:</span> : <span className='noAnswers'>There are no answers</span>}
       {view === 'all'
       ? <div className='whole-answer'>
           {props.answers.map( ans => {
