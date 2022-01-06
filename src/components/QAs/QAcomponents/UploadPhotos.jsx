30f1e74a1b98c00c73ccd41126fb9d335fe9f652
Thanh Ha <thanh3203@gmail.com>
2022-01-06 09:22:33 -0800

    removed the selected photos state and moved it to questions for handling submission

--- src/components/QAs/QAcomponents/UploadPhotos.jsx
+++ src/components/QAs/QAcomponents/UploadPhotos.jsx
@@ -8,33 +8,19 @@ const style = {
 };
 
 const UploadPhotos = (props) => {
-  const [previews, setPreviews] = useState([]);
-
-  const handleSelected = (event) => {
-    let len = Object.keys(event.target.files);
-
-    let files = event.target.files
-    let data = [];
-    for (let i = 0; i < len.length; i++) {
-      let url = URL.createObjectURL(files[len[i]]);
-      data.push(url);
-    };
-    setPreviews([...previews, ...data]);
-  };
 
   return (
     <div className='upload'>
       <label htmlFor='uploadPhotos' className='photoLabel'>
-        <input id='uploadPhotos' type='file' multiple accept='image/*' onChange={handleSelected} style={{display: 'none'}}/>
+        <input id='uploadPhotos' type='file' multiple accept='image/*' onChange={props.task} style={{display: 'none'}}/>
         Select Images
       </label>
       <div>
-        {previews.length > 0
-          ? previews.map(photo => { return <img style={style} className='thumbnail' src={photo}></img>})
+        {props.previews.length > 0
+          ? props.previews.map(photo => { return <img style={style} className='thumbnail' src={photo}></img>})
           : <span>No images selected</span>
         }
       </div>
-      {props.error && <div>Too many pictures</div>}
     </div>
   );
 };
