6d73792f2cf0d05a1b91ef670f16b0c1887a9acb
timnevada <timnevada@gmail.com>
2022-01-06 09:12:15 -0800

    removed console logs

--- server/router/cart.js
+++ server/router/cart.js
@@ -15,11 +15,6 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   let id = Number(req.body.sku_id)
   let itemCount = Number(req.body.count)
-  console.log(req.body)
-  console.log(typeof id)
-  console.log(id)
-  console.log(typeof itemCount)
-  console.log(itemCount)
   api.postCart (id, itemCount, (err, result) => {
     if (err) {
       console.error(err);
