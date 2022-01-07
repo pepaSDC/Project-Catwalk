35322b210da1eaa61f3d7d5bc90cd80825fe7aae
Thanh Ha <thanh3203@gmail.com>
2022-01-07 12:21:05 -0800

    moved the show more button and add question outside of the scrollable content

--- src/components/QAs/QAcomponents/Questions.jsx
+++ src/components/QAs/QAcomponents/Questions.jsx
@@ -22,30 +22,32 @@ const Questions = (props) => {
   };
 
   return (
-    <div className='wholeQuestion'>
-      <QuestionModal open={props.view} onClose={props.handleView} product_name={props.product_name}>
-        <form className='form' onSubmit={props.task} autoComplete='off'>
-          <label>Your Question <span className='asterisk'>*</span></label>
-          <textarea name='body' maxLength='1000' rows='8' placeholder='Why did you like the product or not?' onFocus={props.handleError}></textarea>
-          {!props.errors.body && <div className='error'>Please enter valid answer (max 1000 characters)</div>}
-          <label>What is your nickname <span className='asterisk'>*</span></label>
-          <input className='username' type='text' maxLength='60' name='name' placeholder='Example: jackson11!' onFocus={props.handleError} onBlur={props.handleBlur}></input>
-          {props.focus.name && <div className='privacy'>For privacy reasons, do not use your full name or email address</div>}
-          {!props.errors.name && <div className='error'>Please enter valid name (max 60 characters)</div>}
-          <label>Your Email <span className='asterisk'>*</span></label>
-          <input className='email' type='text' maxLength='60' name='email' placeholder='Example: jack@email.com' onFocus={props.handleError} onBlur={props.handleBlur}></input>
-          {props.focus.email && <div className='privacy'>For authentication reasons, you will not be emailed</div>}
-          {!props.errors.email ? <div className='error'>Please enter an email (max 60 characters)</div> : props.errors.email === 'wrong' && <div className='error'>Please enter a valid email</div>}
-          <input className='questionSubmit' type='submit' value='Answer'></input>
-        </form>
-      </QuestionModal>
-      {questions.map( (question) => {
-        return (
-          <div key={question.question_id} className='oneQ'>
-              <Question question={question} product_name={props.product_name}/>
-          </div>
-        );
-      })}
+    <div>
+      <div className='wholeQuestion'>
+        <QuestionModal open={props.view} onClose={props.handleView} product_name={props.product_name}>
+          <form className='form' onSubmit={props.task} autoComplete='off'>
+            <label>Your Question <span className='asterisk'>*</span></label>
+            <textarea name='body' maxLength='1000' rows='8' placeholder='Why did you like the product or not?' onFocus={props.handleError}></textarea>
+            {!props.errors.body && <div className='error'>Please enter valid answer (max 1000 characters)</div>}
+            <label>What is your nickname <span className='asterisk'>*</span></label>
+            <input className='username' type='text' maxLength='60' name='name' placeholder='Example: jackson11!' onFocus={props.handleError} onBlur={props.handleBlur}></input>
+            {props.focus.name && <div className='privacy'>For privacy reasons, do not use your full name or email address</div>}
+            {!props.errors.name && <div className='error'>Please enter valid name (max 60 characters)</div>}
+            <label>Your Email <span className='asterisk'>*</span></label>
+            <input className='email' type='text' maxLength='60' name='email' placeholder='Example: jack@email.com' onFocus={props.handleError} onBlur={props.handleBlur}></input>
+            {props.focus.email && <div className='privacy'>For authentication reasons, you will not be emailed</div>}
+            {!props.errors.email ? <div className='error'>Please enter an email (max 60 characters)</div> : props.errors.email === 'wrong' && <div className='error'>Please enter a valid email</div>}
+            <input className='questionSubmit' type='submit' value='Answer'></input>
+          </form>
+        </QuestionModal>
+        {questions.map( (question) => {
+          return (
+            <div key={question.question_id} className='oneQ'>
+                <Question question={question} product_name={props.product_name}/>
+            </div>
+          );
+        })}
+      </div>
       {!props.found && <div>No questions found</div>}
       <div className='topBar'>
         {props.questions.length > count
