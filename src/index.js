6ee3c5c8b644091c5c0b63b26744d1cddec3a218
Thanh Ha <thanh3203@gmail.com>
2022-01-06 16:02:55 -0800

    added the click tracking business change functionality. currently commented out to reduce console logs during dev time

--- src/index.js
+++ src/index.js
@@ -2,4 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
+// const traverseParents = (node) => {
+//   if (node.parentNode.id === 'app') {
+//     return;
+//   };
+//   if (node.parentNode.id === 'QAs'
+//       || node.parentNode.id === 'Overview'
+//       || node.parentNode.id === 'Related_Products'
+//       || node.parentNode.id === 'RatingsAndReviews')
+//   {
+//     return node.parentNode.id;
+//   };
+//   if (node.parentNode) {
+//     return traverseParents(node.parentNode);
+//   };
+// };
+
+// const handleClick = (event) => {
+//   var date = new Date();
+//   console.log('TIME: ', date);
+//   let element = event.target;
+//   let found = traverseParents(element);
+//   console.log(found);
+// };
+
+// const app = document.getElementById('app');
+// app.addEventListener('click', handleClick);
+
 ReactDOM.render(<App />, document.getElementById('app'));
